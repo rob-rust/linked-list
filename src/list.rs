ea9019778427f4fcd88194428dd8a0189ebcd099
rongbai <1577973940@qq.com>
2025-06-20 16:45:15 +0800

    add iter_mut feature

--- src/list.rs
+++ src/list.rs
@@ -49,6 +49,12 @@ impl<T> List<T> {
         }
     }
 
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut()
+        }
+    }
+
 }
 
 impl<T> Drop for List<T> {
@@ -86,10 +92,25 @@ impl<'a, T> Iterator for Iter<'a ,T>{
         }
         )
     }
-
 }
 
 
+pub struct IterMut<'a,T>{
+    next: Option<&'a mut Node<T>>
+}
+
+impl <'a,T> Iterator for IterMut<'a,T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(
+            |node| {
+                self.next = node.next.as_deref_mut();
+                &mut node.elem
+            }
+        )
+    }
+
+}
 
 
 #[cfg(test)]
@@ -164,6 +185,17 @@ mod test {
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 
 }
