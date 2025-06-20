91f90126f7dbbc2f69382a43a430f780382e3072
rongbai <1577973940@qq.com>
2025-06-20 14:42:52 +0800

    add iter feature

--- src/list.rs
+++ src/list.rs
@@ -1,3 +1,4 @@
+
 pub struct List<T> {
     head: Link<T>,
 }
@@ -42,6 +43,12 @@ impl<T> List<T> {
         IntoIter(self)
     }
 
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref()
+        }
+    }
+
 }
 
 impl<T> Drop for List<T> {
@@ -62,6 +69,24 @@ impl<T> Iterator for IntoIter<T> {
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop()
     }
+}
+
+pub struct Iter<'a, T>{
+    next: Option<&'a Node<T>>
+}
+
+impl<'a, T> Iterator for Iter<'a ,T>{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node|
+            {
+            self.next = node.next.as_deref();
+                &node.elem
+        }
+        )
+    }
+
 }
 
 
@@ -128,6 +153,17 @@ mod test {
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 
 }
